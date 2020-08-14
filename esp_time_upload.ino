6d92fde46ef047eed75682743d3a7949d1fe3b21
Varsha <66315875+VarshaSBhat@users.noreply.github.com>
2020-08-14 13:07:03 +0530

    Update esp_time_upload.ino

--- esp_time_upload.ino
+++ esp_time_upload.ino
@@ -7,49 +7,37 @@
 #include <LiquidCrystal_I2C.h>
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-
 #ifndef STASSID
-#define STASSID "" //SSiD
-#define STAPSK  "" //Password
+#define STASSID "Belkin.3E62" //SSiD
+#define STAPSK  "D424935F" //Password
 #endif
-
-#define dataPin 0          //pin where the dht11 is connected
+#define dataPin 0          //pin where the dht11 is connected
 #define msensor A0
 char* ssid = STASSID;
 char* password = STAPSK;
-
-String api_key = "";     //  Enter your Write API key from ThingSpeak
+String api_key = "O765GBCGGB8LVJHP";     //  Enter your Write API key from ThingSpeaka
 const char* server = "api.thingspeak.com";
-
 DHT dht(dataPin, DHT11);
-
-
-
 long utcOffsetInSeconds = 5*60*60+60*30; // +5.5 UTC
 int h,m,s;
 float hm,t,sm;
 int flag=0;
 unsigned long previousMillis = 0; 
 const long interval = 1000; 
-
-
-
 WiFiClient client;
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
-
 void setup(){
 Serial.begin(9600);
 dht.begin();
-lcd.init();
+lcd.begin();
 lcd.backlight();
 WiFi.begin(ssid, password);
 lcd.clear();
 lcd.setCursor(0, 0);
 lcd.print("Connecting Wifi");
 lcd.setCursor(0, 1);
-
 while ( WiFi.status() != WL_CONNECTED ) {
 delay ( 500 );
 lcd.print(".");
@@ -58,67 +46,62 @@ Serial.print ( "." );
 timeClient.begin();
 lcd.clear();
 }
-
 void loop() {
-
 unsigned long currentMillis = millis();
 if (currentMillis - previousMillis >= interval) {
 previousMillis = currentMillis;
 hm = dht.readHumidity();
 t = dht.readTemperature();
 sm = analogRead(msensor);
-sm= map(sm, 550,0,0,100);
+//sm= map(sm, 550,0,0,100);
 timeClient.forceUpdate();
 h = timeClient.getHours();
 m = timeClient.getMinutes();
 s = timeClient.getSeconds();
-
-Serial.print(timeClient.getHours());
+/*Serial.print(timeClient.getHours());
 Serial.print(":");
 Serial.print(timeClient.getMinutes());
 Serial.print(":");
-Serial.println(timeClient.getSeconds());
-
+Serial.println(timeClient.getSeconds());*/
 //Serial.println(timeClient.getFormattedTime());
-if((h==12||h==18)&&m==0&&(s==0||s==59||s==1))
+if((h==6||h==12||h==18)&&m==0&&(s==0||s==59||s==1))
 { 
-  if(flag==0)
-  {
-    if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
-    {  
-          lcd.setCursor(0, 0);
-          lcd.print("uploading");   
-          String data_to_send = api_key;
-          data_to_send += "&field1=";
-          data_to_send += hm;
-          data_to_send += "&field2=";
-          data_to_send += t;
-          data_to_send += "&field3=";
-          data_to_send += sm;
-          data_to_send += "\r\n\r\n";
-          client.print("POST /update HTTP/1.1\n");
-          client.print("Host: api.thingspeak.com\n");
-          client.print("Connection: close\n");
-          client.print("X-THINGSPEAKAPIKEY: " + api_key + "\n");
-          client.print("Content-Type: application/x-www-form-urlencoded\n");
-          client.print("Content-Length: ");
-          client.print(data_to_send.length());
-          client.print("\n\n");
-          client.print(data_to_send);
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("uploaded");
-     }
-     flag=1;
-  }  
-   client.stop();  
-}     
-if((h==12||h==18)&&m==15&&(s==0||s==59||s==1))
+  if(flag==0)
+  {
+    if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+    {  
+          lcd.setCursor(0, 0);
+          lcd.print("uploading");   
+          String data_to_send = api_key;
+          data_to_send += "&field1=";
+          data_to_send += hm;
+          data_to_send += "&field2=";
+          data_to_send += t;
+          data_to_send += "&field3=";
+          data_to_send += sm;
+          data_to_send += "\r\n\r\n";
+          client.print("POST /update HTTP/1.1\n");
+          client.print("Host: api.thingspeak.com\n");
+          client.print("Connection: close\n");
+          client.print("X-THINGSPEAKAPIKEY: " + api_key + "\n");
+          client.print("Content-Type: application/x-www-form-urlencoded\n");
+          client.print("Content-Length: ");
+          client.print(data_to_send.length());
+          client.print("\n\n");
+          client.print(data_to_send);
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("uploaded");
+     }
+     flag=1;
+  }  
+   client.stop();  
+}     
+if((h==6||h==12||h==18)&&m==15&&(s==0||s==59||s==1))
 {
-  lcd.clear();
-  flag=0;
+  lcd.clear();
+  flag=0;
 }
-
 lcd.setCursor(12, 0);
 if (h <= 9){
 lcd.print("0");
@@ -137,7 +120,6 @@ lcd.print("0");
 lcd.print(s);}
 else{
 lcd.print(s);}
-
 lcd.setCursor(0, 1);
 lcd.print("Temperature: ");
 lcd.setCursor(13, 1);
@@ -152,6 +134,6 @@ lcd.setCursor(0, 3);
 lcd.print("Moisture: ");
 lcd.setCursor(13, 3);
 lcd.print(sm);
-lcd.print("%");
+//lcd.print("%");
 }
 }
